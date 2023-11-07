40d297d8fd598b6ea408b0b057ef131151fadfd2
Bart Wise <bart.c.wise@gmail.com>
2023-11-06 19:38:50 -0700

    Added the window-picker plugin so that opening a file in neo-tree, you can choose which window to put the file.

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -402,4 +402,19 @@ return {
       }
     end,
   },
-} -- Return Plugins
+
+  --------------------------------------------------------------------
+  -- PLUGIN: Window-Picker
+  -- Github: s1n7ax/nvim-window-picker
+  -- Comment: Adding clang options.
+  --------------------------------------------------------------------
+  {
+    "s1n7ax/nvim-window-picker",
+    lazy = false,
+    name = "window-picker",
+    version = "2.*",
+    config = function()
+      require("window-picker").setup()
+    end,
+  },
+} -- Return Plugins  !
