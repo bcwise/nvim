a6e0b3f1b97f005cd16d568103412db36ceaa75e
Bart Wise <bart.c.wise@gmail.com>
2024-03-18 19:50:24 -0600

    LazyVim initial checkin

--- lua/plugins/nvim-treesitter-context.lua
+++
@@ -1,8 +0,0 @@
---------------------------------------------------------------------------------
--- Plugin: nvim-treesitter-context
--- Comments: Show context of the current function
---------------------------------------------------------------------------------
-return {
-  "nvim-treesitter/nvim-treesitter-context",
-  opts = { mode = "cursor", max_lines = 3 },
-}
