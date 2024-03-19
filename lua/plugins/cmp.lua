254097ca852be3f943582c736523f2a4113a94fa
Bart Wise <bart.c.wise@gmail.com>
2024-03-18 22:07:20 -0600

    Removed the nvim- and vim- prefixes from file names. Added lspkind (thanks to someone on the internet).

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -74,6 +74,35 @@ return {
   ------------------------------------------------------------------------------
   -- Adding lspkind
   ------------------------------------------------------------------------------
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "onsails/lspkind.nvim",
+    },
+    opts = function(_, opts)
+      opts.window = {
+        completion = {
+          winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+          col_offset = -3,
+          side_padding = 0,
+        },
+      }
+
+      opts.formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, item)
+          local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, item)
+
+          local strings = vim.split(kind.kind, "%s", { trimempty = true })
+          kind.kind = " " .. (strings[1] or "") .. " "
+          kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+          return kind
+        end,
+      }
+    end,
+  },
+
   -- {
   --   "nvim-cmp",
   --
