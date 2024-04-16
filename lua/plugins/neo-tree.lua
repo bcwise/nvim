eff93091973ddee64eef9cb653f62ecb0529883d
Bart Wise <bart.c.wise@gmail.com>
2024-04-16 16:17:52 -0600

    Added vim-illuminate.lua to highlight words matching the word under the cursor.

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,48 @@
+--------------------------------------------------------------------
+-- PLUGIN:  neo-treenvim
+-- GitHub:  nvim-neo-tree/neo-treenvim
+-- Comment: Neovim plugin to browse the file system and other tree-like
+--          structures.
+--------------------------------------------------------------------
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  dependencies = {
+    { "s1n7ax/nvim-window-picker" },
+  },
+  opts = {
+    sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+    open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
+    },
+    window = {
+      mappings = {
+        ["<space>"] = "none",
+        ["Y"] = {
+          function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            vim.fn.setreg("+", path, "c")
+          end,
+          desc = "Copy Path to Clipboard",
+        },
+        ["O"] = {
+          function(state)
+            require("lazy.util").open(state.tree:get_node().path, { system = true })
+          end,
+          desc = "Open with System Application",
+        },
+      },
+    },
+    default_component_configs = {
+      indent = {
+        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+        expander_collapsed = "",
+        expander_expanded = "",
+        expander_highlight = "NeoTreeExpander",
+      },
+    },
+  },
+} -- Return Plugins  !
