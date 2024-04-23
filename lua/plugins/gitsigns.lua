bd92d96d9a672a9f89bbaf1061acec0b0c108e1f
Bart Wise <bart.c.wise@gmail.com>
2024-04-23 00:27:33 -0600

    Updated gitsigns config.

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -12,7 +12,51 @@ return {
       },
       on_attach = function(buffer)
         local gitsigns = package.loaded.gitsigns
+        local border = {
+          { "🭽", "FloatBorder" },
+          { "▔", "FloatBorder" },
+          { "🭾", "FloatBorder" },
+          { "▕", "FloatBorder" },
+          { "🭿", "FloatBorder" },
+          { "▁", "FloatBorder" },
+          { "🭼", "FloatBorder" },
+          { "▏", "FloatBorder" },
+        }
+        gitsigns.signcolumn = true
+        gitsigns.numhl = false -- Toggle with `:Gitsigns toggle_numhl`
+        gitsigns.linehl = false -- Toggle with `:Gitsigns toggle_linehl`
+        gitsigns.word_diff = false -- Toggle with `:Gitsigns toggle_word_diff`
+        gitsigns.watch_gitdir = {
+          interval = 1000,
+          follow_files = true,
+        }
+        gitsigns.attach_to_untracked = true
 
+        gitsigns.preview_config = {
+          -- Options passed to nvim_open_win
+          border = "single",
+          style = "minimal",
+          relative = "cursor",
+          row = 0,
+          col = 1,
+        }
+        ----------------------------
+        -- Blame line settings
+        ----------------------------
+        gitsigns.current_line_blame = true -- Toggle with `:Gitsigns toggle_current_line_blame`
+        gitsigns.current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+          delay = 1000,
+          ignore_whitespace = false,
+        }
+        gitsigns.current_line_blame_formatter_opts = {
+          relative_time = false,
+        }
+
+        ----------------------------
+        -- Mapping
+        ----------------------------
         local function map(mode, l, r, opts)
           opts = opts or {}
           opts.buffer = bufnr
@@ -66,26 +110,6 @@ return {
   },
 }
 
--- local function map(mode, l, r, desc)
---   vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
--- end
---
--- -- stylua: ignore start
--- map("n", "]h", gs.next_hunk, "Next Hunk")
--- map("n", "[h", gs.prev_hunk, "Prev Hunk")
--- map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
--- map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
--- map("n", "<leader>hS", gs.stage_buffer, "Stage Buffer")
--- map("n", "<leader>hu", gs.undo_stage_hunk, "Undo Stage Hunk")
--- map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
--- map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
--- map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
--- map("n", "<leader>ghd", gs.diffthis, "Diff This")
--- map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
--- map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
--- end,
---   {
---
 --     "lewis6991/gitsigns.nvim",
 --     opts = {
 --       ----------------------------
