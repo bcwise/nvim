11c5565eefc41037a279414f0bd34a638e3ff84b
Bart Wise <bart.c.wise@gmail.com>
2024-05-07 13:28:19 -0600

    Removed duplicate variable names and definitions.

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,29 +2,6 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
-      formatters_by_ft = {
-        cpp = { "clang-format" },
-        c = { "clang-format" },
-        lua = { "stylua" },
-        python = { "black" },
-        sh = { "shfmt" },
-      },
-
-      ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
-      formatters = {
-        injected = { options = { ignore_errors = true } },
-        -- # Example of using dprint only when a dprint.json file is present
-        -- dprint = {
-        --   condition = function(ctx)
-        --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
-        --   end,
-        -- },
-        --
-        -- # Example of using shfmt with extra args
-        shfmt = {
-          prepend_args = { "-i", "2", "-ci" },
-        },
-      },
       ---@type table<string, conform.FormatterUnit[]>
       formatters_by_ft = {
         c = { "clang-format" },
@@ -49,6 +26,21 @@ return {
         handlebars = { "prettier" },
       },
 
+      ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
+      formatters = {
+        injected = { options = { ignore_errors = true } },
+        -- # Example of using dprint only when a dprint.json file is present
+        -- dprint = {
+        --   condition = function(ctx)
+        --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
+        --   end,
+        -- },
+        --
+        -- # Example of using shfmt with extra args
+        shfmt = {
+          prepend_args = { "-i", "2", "-ci" },
+        },
+      },
       --he options you set here will be merged with the builtin formatters.
       -- You can also define any custom formatters here.
     },
