5d3aa944f14be07669e8321ff63c00efbbf6bc7c
Bart Wise <bart.c.wise@gmail.com>
2024-05-07 15:00:00 -0600

    The colors were all commented out. I put the colors back in.

--- lua/plugins/fluoromachine.lua
+++ lua/plugins/fluoromachine.lua
@@ -31,18 +31,18 @@ return {
           ["@field"] = { italic = true },
           ["@parameter"] = { italic = true },
         },
-        -- colors = function(_, d)
-        --   return {
-        --     -- bg = '#190920',
-        --     -- alt_bg = d('#190920', 20),
-        --     cyan = '#49eaff',
-        --     -- red = '#ff1e34',
-        --     -- yellow = '#ffe756',
-        --     -- orange = '#f38e21',
-        --     -- pink = '#ffadff',
-        --     -- purple = '#9544f7',
-        --   }
-        -- end,
+        colors = function(_, d)
+          return {
+            bg = "#190920",
+            alt_bg = d("#190920", 20),
+            cyan = "#49eaff",
+            red = "#ff1e34",
+            yellow = "#ffe756",
+            orange = "#f38e21",
+            pink = "#ffadff",
+            purple = "#9544f7",
+          }
+        end,
       })
 
       vim.cmd.colorscheme("fluoromachine")
