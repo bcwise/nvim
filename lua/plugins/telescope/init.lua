65f36f7c03402b208e81bf2e06b98e19ff234b99
Bart Wise <bart.c.wise@gmail.com>
2024-05-15 17:30:39 -0600

    Various telescope fixes.

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -2,4 +2,5 @@ return {
   require("plugins.telescope.telescope"),
   require("plugins.telescope.undo"),
   require("plugins.telescope.adjacent"),
+  require("plugins.telescope.lsp-toggle"),
 }
--- lua/plugins/telescope/telescope.lua
+++ lua/plugins/telescope/telescope.lua
@@ -1,25 +1,112 @@
 return {
-  -- change some telescope options and a keymap to browse plugin files
-  {
-    "nvim-telescope/telescope.nvim",
-    lazy = false,
-    keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
-    },
-    -- change some options
-    opts = {
+  "nvim-telescope/telescope.nvim",
+  cmd = "Telescope",
+  -- version = "0.1.x",
+  version = false,
+  dependencies = {
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    { "tsakirist/telescope-lazy.nvim" },
+  },
+  config = function()
+    local actions = require("telescope.actions")
+    local trouble = require("trouble.providers.telescope")
+    local open_with_trouble = require("trouble.sources.telescope").open
+
+    require("telescope").setup({
       defaults = {
         layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
+        layout_config = {
+          horizontal = { prompt_position = "top", width = 0.99, height = 0.99, preview_width = 0.7 },
+          -- sorting_strategy = "ascending",
+        },
+        prompt_prefix = "🔍 ",
+        borderchars = {
+          { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+          prompt = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+          results = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+          preview = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+        },
+        set_env = { ["COLORTERM"] = "truecolor" },
+        vimgrep_arguments = {
+          "rg",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+          "--hidden",
+          "--glob=!.git/",
+        },
+        file_ignore_patterns = {
+          ".git/",
+          ".cache",
+          ".next",
+          "%.o",
+          "%.a",
+          "%.out",
+          "%.class",
+          "%.pdf",
+          "%.mkv",
+          "%.mp4",
+          "%.zip",
+          "node_modules",
+          "target",
+        },
+        path_display = { truncate = 5 },
+        hidden = true,
+        color_devicons = true,
+        mappings = {
+          i = {
+            ["<C-p>"] = actions.preview_scrolling_up,
+            ["<c-t>"] = open_with_trouble,
+            ["<C-n>"] = actions.preview_scrolling_down,
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+          },
+          n = {
+            ["q"] = actions.close,
+            ["<C-c>"] = actions.close,
+            ["<c-t>"] = open_with_trouble,
+          },
+        },
       },
-    },
-  },
+
+      pickers = {
+        find_files = {
+          find_command = { "fd", "--no-ignore", "--type", "f", "--strip-cwd-prefix", "--color=auto" },
+          hidden = true,
+        },
+        live_grep = {
+          additional_args = { "--hidden", "--ignore-case" },
+          only_sort_text = true,
+        },
+        buffers = { path_display = { "tail" } },
+        colorscheme = {
+          enable_preview = true,
+        },
+      },
+
+      extensions = {
+        rooter = {
+          enable = true,
+          patterns = { ".git", "node_modules" },
+          debug = false,
+        },
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        },
+      },
+    })
+
+    local extension = require("telescope").load_extension
+
+    extension("fzf")
+    extension("projects")
+    extension("lazy")
+    extension("notify")
+  end,
 }
