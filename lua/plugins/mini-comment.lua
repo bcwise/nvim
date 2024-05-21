d199af1e43a2e1a0b87611c842290955c98ce860
Bart Wise <bart.c.wise@gmail.com>
2024-05-20 23:31:03 -0600

    Moved from comment-nvim to mini-comment.

--- lua/plugins/mini-comment.lua
+++ lua/plugins/mini-comment.lua
@@ -1,4 +1,16 @@
 return {
-  -- disable
-  { "echasnovski/mini.comment", enabled = false },
+  {
+    "echasnovski/mini.comment",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        commentstring = function()
+          return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+        end,
+        custom_commentstring = function()
+          return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+        end,
+      },
+    },
+  },
 }
