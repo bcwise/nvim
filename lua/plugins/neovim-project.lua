10c832961c22cfde0f0f2d86668fa7a829596d29
Bart Wise <bart.c.wise@gmail.com>
2024-05-21 01:50:08 -0600

    Added new project manager.

--- lua/plugins/neovim-project.lua
+++ lua/plugins/neovim-project.lua
@@ -1,23 +1,33 @@
 return {
-{
-  "coffebar/neovim-project",
-  opts = {
-    projects = { -- define project roots
-      "~/projects/*",
-      "~/dev/*",
-      "~/.config/*",
+  {
+    "coffebar/neovim-project",
+    opts = {
+      projects = { -- define project roots
+        "~/projects/*",
+        "~/dev/*",
+        "~/.config/*",
+        "~/Documents/church",
+        "~/Documents/church/general_conference",
+      },
+      -- Load the most recent session on startup if not in the project directory
+      last_session_on_startup = false,
+      -- Dashboard mode prevent session autoload on startup
+      dashboard_mode = false,
     },
+    init = function()
+      -- enable saving the state of plugins in the session
+      vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+    end,
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim", tag = "0.1.4" },
+      { "Shatur/neovim-session-manager" },
+    },
+    lazy = false,
+    priority = 100,
+
+    keys = { { "<leader>tp", "<cmd>Telescope neovim-project<cr>", desc = "Project Management" } },
+    keys = { { "<leader>tnd", "<cmd>Telescope neovim-project discovery<cr>", desc = "Project Management discovery" } },
+    keys = { { "<leader>tnh", "<cmd>Telescope neovim-project history<cr>", desc = "Project Management history" } },
   },
-  init = function()
-    -- enable saving the state of plugins in the session
-    vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
-  end,
-  dependencies = {
-    { "nvim-lua/plenary.nvim" },
-    { "nvim-telescope/telescope.nvim", tag = "0.1.4" },
-    { "Shatur/neovim-session-manager" },
-  },
-  lazy = false,
-  priority = 100,
-},
 }
