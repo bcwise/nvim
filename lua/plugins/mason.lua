541c9aa9811e6b23bbcc16ba8e053c1b75373185
Bart Wise <bart.c.wise@gmail.com>
2024-05-23 12:48:18 -0600

    Cleaned up mason.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,17 +2,35 @@
 return {
   {
     "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup({
-        ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-          },
-        },
-      })
-    end,
+    cmd = "Mason",
+    opts = {
+      ensure_installed = {
+        "bash-language-server",
+        "black",
+        "cmake-language-server",
+        "json-lsp",
+        "lua-language-server",
+        "ruby-lsp",
+        "shellcheck",
+        "shfmt",
+        "sqls",
+        "stylua",
+        "texlab",
+        "typescript-language-server",
+      },
+
+      -- config = function()
+      --   require("mason").setup({
+      --     ui = {
+      --       icons = {
+      --         package_installed = "✓",
+      --         package_pending = "➜",
+      --         package_uninstalled = "✗",
+      --       },
+      --     },
+      --   })
+      -- end,
+    },
   },
 
   {
@@ -21,45 +39,4 @@ return {
       require("mason-lspconfig").setup()
     end,
   },
-
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    config = function()
-      require("mason-tool-installer").setup({
-        ensure_installed = {
-          "bash-language-server",
-          "black",
-          "cmake-language-server",
-          "json-lsp",
-          "lua-language-server",
-          "marksman",
-          "prettier",
-          "ruby-lsp",
-          "rust-analyzer",
-          "shellcheck",
-          "sqls",
-          "stylua",
-          "texlab",
-          "typescript-language-server",
-        },
-      })
-    end,
-  },
-
-  -- {
-  --   "williamboman/mason.nvim",
-  --   opts = {
-  --     ensure_installed = {
-  --       "bash-language-server",
-  --       "cmake-language-server",
-  --       "json-lsp",
-  --       "lua-language-server",
-  --       "ruby-lsp",
-  --       "shellcheck",
-  --       "sqls",
-  --       "texlab",
-  --       "typescript-language-server",
-  --     },
-  --   },
-  -- },
 }
