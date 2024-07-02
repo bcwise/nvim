96805b9ced47770274cac2cf14c3296611e46aa9
Bart Wise <bart.c.wise@gmail.com>
2024-07-02 12:34:26 -0600

    Added the <leader>o keymap to open up aerial (displays an outline of symbols).  Added cheatsheet codes for Aerial.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -94,8 +94,8 @@ map(
 --* Comments: A code outline window for skimming and quick navigation.
 --*           The key mapping is so we can use <leader>o to toggle the outline
 --*           viewer open and close.
---********************************************************************kk**********
--- map({ "n" }, "<leader>o", "<cmd>AerialToggle!<CR>", { desc = "Opens/Closes a code outline window." })
+--********************************************************************************
+map({ "n" }, "<leader>o", "<cmd>AerialToggle!<CR>", { desc = "Opens/Closes a code outline window." })
 
 --******************************************************************************
 --* PLUGIN: Easy Align
