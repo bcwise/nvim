60af501ae156c209fc71ec6fb2a75ca74f926f4d
Bart Wise <bart.c.wise@gmail.com>
2024-07-02 13:53:52 -0600

    Put in a placeholder indicating that the default config is coming from LazyVim.

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -2,3 +2,6 @@
 -- Aerial is being loaded by the config/lazy file.
 -- If we need further configuration, that should go here.   But as it is,
 -- we will get the default configuration.
+return {
+  {},
+}
